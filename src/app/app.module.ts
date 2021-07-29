bede6a4e8ba323faee67fbfe0874a4d505cdc68a
MariaZabolotskih <46151115+MariaZabolotskih@users.noreply.github.com>
2021-07-29 19:14:31 +0300

    Сделаны task-3 и task-1, task-2 частично

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabeComponent } from './tabe/tabe.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule,  } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+import { StartPageComponent } from './start-page/start-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatInputModule } from '@angular/material/input';
+import { StreamsComponent } from './streams/streams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabeComponent,
+    StartPageComponent,
+    StreamsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatButtonModule,
+    OverlayModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/streams/streams.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, merge, timer } from 'rxjs';
+import { skipUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-streams',
+  templateUrl: './streams.component.html',
+  styleUrls: ['./streams.component.scss']
+})
+
+export class StreamsComponent implements OnInit {
+
+  private _sum = 0;
+  private _isDisabled = false;
+  private _formData: FormData[] = [
+    {title: "Первый поток", streamNumber: 0, streamIds: []},
+    {title: "Второй поток", streamNumber: 1, streamIds: []},
+    {title: "Третий поток", streamNumber: 2, streamIds: []},
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get sum() {
+    return this._sum;
+  } 
+
+  get isDisabled() {
+    return this._isDisabled;
+  }
+
+  get formData() {
+    return this._formData;
+  }
+
+
+  public run() {
+    this.clear();
+    this._isDisabled = true;
+
+    const stream1 = this.createStream(0, 1000);
+    const stream2 = this.createStream(1, 1500);
+    const stream3 = this.createStream(2, 2000);
+
+    const stream4 = merge(
+      stream1, 
+      stream2.pipe(skipUntil(timer(10000))), 
+      stream3.pipe(skipUntil(timer(20000))),  
+    ).subscribe((val: StreamObject) => {
+      this._sum += val.id;
+      this._formData[val.stream].streamIds.push(val.id);
+    }) 
+    
+    setTimeout(()=> {
+      stream4.unsubscribe();
+      this._isDisabled = false;
+    }, 30000);
+
+  }
+
+  private createStream(streamNumber: number, time: number) {
+    return new Observable<StreamObject>((sub) => {
+      let counter = 1;
+      const interval = setInterval(() => sub.next({id: counter++, stream: streamNumber}), time);
+
+      return () => {
+        clearInterval(interval);
+      }
+    });
+  }
+
+  private clear() {
+    this._sum = 0;
+    this._formData[0].streamIds = [];
+    this._formData[1].streamIds = [];
+    this._formData[2].streamIds = [];
+  }
+ 
+}
+
+interface StreamObject {
+  id: number; 
+  stream: number;
+}
+
+interface FormData {
+  title: string;
+  streamNumber: number;
+  streamIds: number[],
+}
